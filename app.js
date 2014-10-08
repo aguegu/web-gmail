496272274a114dc8ca0d1530a61039222349c0be
Weihong Guan <weihong.guan@gmail.com>
2014-10-08 17:58:48 +0800

    init gmail router

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var routerGmail = require('./routes/gmail');
 
 var app = express();
-
 
 Db = require('mongodb').Db,
 Connection = require('mongodb').Connection,
@@ -22,7 +23,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -30,7 +31,17 @@ app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+var MongoStore = require('connect-mongo')(session);
+app.use(session({
+  secret: "development",
+  cookie: {maxAge: 1000 * 60 * 60 * 24 * 14}, // 14 days
+  store: new MongoStore({
+    db: "web-gmail-session"
+  })
+}));
+
+// app.use('/', routes);
+// app.use('/', routerGmail);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
---
+++ routes/gmail.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+// var google = require('googleapis');
+// var OAuth2 = google.auth.OAuth2;
+
+// var cs = require('../client_secret.json');
+// var oauth2Client = new OAuth2(cs.client_id, cs.client_secret, cs.redirect_url);
+
+// var url = oauth2Client.generateAuthUrl({
+//   access_type: 'offline', // 'online' (default) or 'offline' (gets refresh_token)
+//   approval_prompt: 'force',
+//   scope: [
+//     'https://www.googleapis.com/auth/plus.profile.emails.read',
+//     'https://mail.google.com/',
+//   ]
+// });
+
+// router.get('/login', function (req, res) {
+//   res.redirect(url);
+// });
+
+// router.get('/login/authorized', function (req, res) {
+//   // console.log(req);
+// });
+
+
+module.exports = router;
